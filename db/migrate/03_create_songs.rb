1bf7d791601cc690f56f91a34143d129cb36e763
cassymarie <cassy2205@gmail.com>
2020-11-20 09:10:10 -0700

    commit tree 1de7661e21c967fd438024d967a0861ca7744433

--- db/migrate/03_create_songs.rb
+++ db/migrate/03_create_songs.rb
@@ -1,2 +1,9 @@
 class CreateSongs < ActiveRecord::Migration[4.2]
+     def change
+          create_table :songs do |tbl|
+               tbl.string :name
+               tbl.integer :artist_id
+               tbl.integer :genre_id
+          end
+     end
 end
